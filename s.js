3b0ac7c956951215ac09012654df0b540dfdfc7c
Quanpham102 <118402956+Quanpham102@users.noreply.github.com>
2022-11-23 22:52:36 +0700

    Update s.js

--- s.js
+++ s.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 3000;
 const INDEX = '/cl.html';
-
+express= require('express')
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
