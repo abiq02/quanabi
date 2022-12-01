2a792d36f03211d2b32f26f777105512429002e8
Quanpham102 <118402956+Quanpham102@users.noreply.github.com>
2022-12-01 18:05:34 +0700

    Update cl.js

--- cl.js
+++ cl.js
@@ -8,3 +8,20 @@ ws.onmessage = function (event) {
 };
 //123
 _ga=GA1.2.1109528843.1669732705;_gid=GA1.2.2034535752.1669890516;_gat_UA-238090400-1=1;_gat_UA-238090400-2=1
+Ssion
+GET https://api-gw.gwbchind.com/esms/v1/profiler?env=w79
+https://ipv4.colgagnez.com/sw/collect
+https://ipv6.colgagnez.com/sw/collect
+GET json
+ https://api-gw.gwbchind.com/gwms/v1/verifytoken.aspx?token=d7a403e885510a4e7196febb70b20023&fg=9e9045841e5b24cd18b902c91c64c619
+WebSocket
+wss://api-card.bwbay9sockg.com/websocket
+[1,"Simms","","",{"agentId":"1","accessToken":"37-59b15392197f421530175706e6824387","reconnect":false}]
+["7", "Simms", "1",1] 2 10
+[6,"Simms","channelPlugin",{"cmd":310}]
+[6,"Simms","channelPlugin",{"cmd":310}]
+
+wss://api-mini.bwbay9sockg.com/websocket
+[1,"MiniGame","","",{"agentId":"1","accessToken":"37-59b15392197f421530175706e6824387","reconnect":false}]
+[6,"MiniGame","lobbyPlugin",{"cmd":10001}]
+["7", "MiniGame", "1",1]
