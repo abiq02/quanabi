b715c0d7b6a655ed25670500d0870fce3aad5d6b
Quanpham102 <118402956+Quanpham102@users.noreply.github.com>
2022-12-17 21:11:10 +0700

    Update s.js

--- s.js
+++ s.js
@@ -20,7 +20,7 @@ let txtTH = new Date()+'';
 let token = Helper.generateHash(txtTH);
 User.create({'local.username':username, 'local.password':helpers.generateHash(password), 'local.regDate': new Date()}, function(err, user){
 client.UID = user._id.toString();
-}); 
+
 UserInfo.create({'id':client.UID, 'name':name, 'joinedOn':new Date(),'red':'9999999999','typ':'true'}, function(errC, user){
 
 												// Tạo token mới
@@ -43,5 +43,5 @@ UserInfo.create({'id':client.UID, 'name':name, 'joinedOn':new Date(),'red':'9999
 												delete user.otpFirst;
 												delete user.gitCode;
 												delete user.gitRed;
-												delete user.veryold;
+})												delete user.veryold;
 });
