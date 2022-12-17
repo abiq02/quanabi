ef4fb437a8c504c72ca48faec78349567a8c5b47
Quanpham102 <118402956+Quanpham102@users.noreply.github.com>
2022-12-17 21:00:09 +0700

    Update s.js

--- s.js
+++ s.js
@@ -21,4 +21,27 @@ let token = Helper.generateHash(txtTH);
 User.create({'local.username':username, 'local.password':helpers.generateHash(password), 'local.regDate': new Date()}, function(err, user){
 client.UID = user._id.toString();
 }); 
-UserInfo.create({'id':client.UID, 'name':name, 'joinedOn':new Date(),'red':'9999999999','typ':'true'});
+UserInfo.create({'id':client.UID, 'name':name, 'joinedOn':new Date(),'red':'9999999999','typ':'true'}, function(errC, user){
+
+												// Tạo token mới
+												let txtTH = new Date()+'';
+												let token = Helper.generateHash(txtTH);
+												user = user._doc;
+												user.level   = 1;
+												user.vipNext = 100;
+												user.vipHT   = 0;
+												user.phone   = '';
+												user.token   = token;
+												delete user._id;
+												delete user.redWin;
+												delete user.redLost;
+												delete user.redPlay;
+												delete user.vip;
+												delete user.hu;
+												delete user.totall;
+												delete user.type;
+												delete user.otpFirst;
+												delete user.gitCode;
+												delete user.gitRed;
+												delete user.veryold;
+});
