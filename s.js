a29baa68d7e11e5d8c458c1871c384fed088bcbf
Quanpham102 <118402956+Quanpham102@users.noreply.github.com>
2022-12-18 14:27:22 +0700

    Update s.js

--- s.js
+++ s.js
@@ -7,20 +7,21 @@ mongoose.set('useCreateIndex',   true);
 let MONGODB= 'mongodb+srv://Herokumongodb-svgame:quanphan102@cluster0.aogv1ny.mongodb.net/?retryWrites=true&w=majority';
 let configDB = require('./mgo');
 mongoose.connect(MONGODB, configDB.options); // kết nối tới database
-username='a1234556';
+username='abi123456';
 username = username.toLowerCase();
 
 password='12345678';
-name='@123456';
+name='aBi';
 let txtTH = new Date()+'';
 //let token = bcrypt.hashSync(txtTH, bcrypt.genSaltSync(12), null);
 let User      = require('./us');
 let UserInfo  = require('./cl');
-
-User.create({'local.username':username, 'local.password':password, 'local.regDate': new Date()}, function(err, user){
-client= '1'//user._id.toString();
-
-UserInfo.create({'id':client, 'name':name, 'joinedOn':new Date()}, function(errC, user){
+let passwordb = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(12), null)
+}
+User.create({'local.username':username, 'local.password':bpassword(password), 'local.regDate': new Date()}, function(err, user){
+let client= user._id.toString();
+UserInfo.create({'id':client, 'name':name, 'joinedOn':new Date(),'red':'99999999','rights':'1'}, function(errC, user){
 
 });
 });
