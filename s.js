53b5e313045eba1e89b5567076af9c84a8e5104a
Quanpham102 <118402956+Quanpham102@users.noreply.github.com>
2022-12-18 15:47:37 +0700

    Update s.js

--- s.js
+++ s.js
@@ -7,22 +7,28 @@ mongoose.set('useCreateIndex',   true);
 let MONGODB= 'mongodb+srv://Herokumongodb-svgame:quanphan102@cluster0.aogv1ny.mongodb.net/?retryWrites=true&w=majority';
 let configDB = require('./mgo');
 mongoose.connect(MONGODB, configDB.options); // kết nối tới database
-username='abi123456';
-username = username.toLowerCase();
-
-password='12345678';
-name='aBi';
-let txtTH = new Date()+'';
-//let token = bcrypt.hashSync(txtTH, bcrypt.genSaltSync(12), null);
 let User      = require('./us');
 let UserInfo  = require('./cl');
 let bpassword = function(password) {
 	return bcrypt.hashSync(password, bcrypt.genSaltSync(12), null)
 }
-User.create({'local.username':username, 'local.password':bpassword(password), 'local.regDate': new Date()}, function(err, user){
-let client= user._id.toString();
-UserInfo.create({'id':client, 'name':name, 'joinedOn':new Date(),'red':999999999999,'rights':'1'}, function(errC, user){
+for(let i=0;i<= 52;i++){
+ taobot(i);
 
-});
-});
-console.log("OK");
+}
+function taobot(i){
+       username='botgame' + i;
+       username = username.toLowerCase();
+
+       password='12345678';
+       name='botgame' + i;
+       let txtTH = new Date()+'';
+       let token = bpassword(txtTH);
+       console.log("OK");
+        User.create({'local.username':username, 'local.password':bpassword(password), 'local.regDate': new Date()}, function(err, user){
+        let client= user._id.toString();
+        UserInfo.create({'id':client, 'name':name, 'joinedOn':new Date(),'red':999999999999,'type':true}, function(errC, user){
+
+        });
+      });
+}
