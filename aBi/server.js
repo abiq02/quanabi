9c161eb06ebdafede7bb299da64b4da68913aeab
abiq02 <118402956+abiq02@users.noreply.github.com>
2023-08-19 10:19:49 +0700

    Update server.js

--- aBi/server.js
+++ aBi/server.js
@@ -1,1 +1,9 @@
+let express       = require('express');
+let app           = express();
+let port       = process.env.PORT || 80;
+let expressWs  = require('express-ws')(app);
+app.use(express.static('public/'));
+app.listen(port, function() {
+    console.log("Server listen on port ", port);
+});
 console.log("connect");
