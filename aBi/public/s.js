2cc2ee0ebb0125b531cb467483be553c3d92cfa5
abiq02 <118402956+abiq02@users.noreply.github.com>
2023-08-20 17:15:10 +0700

    Add files via upload

---
+++ aBi/public/s.js
@@ -0,0 +1,19 @@
+const click = document.querySelector("#click");
+const mes = document.querySelector("#mes");
+const ms = document.querySelector("#mge");
+click.onclick = function(){
+  a = ms.value;
+  mes.textContent += `\n\n${a}`;
+  mes.scrollTop = mes.scrollHeight;
+  ws.send(a);
+}
+var ws = new WebSocket("wss://bzzhh.onrender.com/");
+    ws.onopen = function (a){
+      console.log("hello");
+    };
+    
+    ws.onmessage = function(message){
+      console.log(message.data);
+      mes.textContent += `\n\n${message.data}`;
+      mes.scrollTop = mes.scrollHeight;
+    };
