23dbcbbd49854a7c94d553053d9aac22a930e525
abiq02 <118402956+abiq02@users.noreply.github.com>
2023-08-27 08:48:11 +0700

    Update s.js

--- aBi/public/s.js
+++ aBi/public/s.js
@@ -1,17 +1,26 @@
-const click = document.querySelector("#click");
+const label = document.querySelector("#mge");
 const mes = document.querySelector("#mes");
-const ms = document.querySelector("#mge");
-click.onclick = function(){
-  a = ms.value;
-  ws.send(a);
-}
-var ws = new WebSocket("wss://bzzhh.onrender.com/");
-    ws.onopen = function (a){
-      console.log("hello");
+const ms = document.querySelector("#inn");
+
+const ws = new WebSocket("wss://bzzhh.onrender.com/");
+
+  ws.onopen = function (a){
+    console.log("hello");
     };
     
-    ws.onmessage = function(message){
-      console.log(message.data);
-      mes.textContent += `\n\n${message.data}`;
-      mes.scrollTop = mes.scrollHeight;
+  ws.onmessage = function(message){
+    message = message.data;
+    json = JSON.parse(message);
+    switch(json[0]){
+      case 1 :
+        mes.textContent += `\n\n${json[1]}`;
+        mes.scrollTop = mes.scrollHeight;
+      break;
+      case 2:                    
+        ms.innerHTML = json[1];
+      break;
+      case 3:
+        label.innerHTML = json[1];
+      break;
+    }
     };
