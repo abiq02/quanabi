ccc2d58ab37807da684b8560f96733be49a7417e
abiq02 <118402956+abiq02@users.noreply.github.com>
2023-08-31 12:03:17 +0700

    Update s.js

--- aBi/public/s.js
+++ aBi/public/s.js
@@ -3,15 +3,14 @@ const mes = document.querySelector("#mes");
 const ms = document.querySelector("#inn");
 
 const ws = new WebSocket("wss://bzzhh.onrender.com/websocket");
-
-  ws.onopen = function (a){
+ws.onopen = function (a){
     console.log("hello");
     };
     
-  ws.onmessage = function(message){
+ws.onmessage = function(message){
     message = message.data;
     json = JSON.parse(message);
-    ws.send('@@')
+    ws.send(' ');
     switch(json[0]){
       case 1 :
         mes.textContent += `\n\n${json[1]}`;
@@ -25,3 +24,7 @@ const ws = new WebSocket("wss://bzzhh.onrender.com/websocket");
       break;
     }
     };
+
+ws.onclose = function(){
+  alert('websocket close');
+}
