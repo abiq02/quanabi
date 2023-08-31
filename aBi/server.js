f620513f74d8a8865e608fcbf21ae165c2dd1cdf
abiq02 <118402956+abiq02@users.noreply.github.com>
2023-08-31 13:10:08 +0700

    Update server.js

--- aBi/server.js
+++ aBi/server.js
@@ -3,6 +3,9 @@ let app           = express();
 let port       = process.env.PORT || 80;
 let expressWs  = require('express-ws')(app);
 app.use(express.static('public/'));
+const TelegramBot = require('node-telegram-bot-api');
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const bot = new TelegramBot(token, {polling: true});
 let wss = expressWs.getWss();
 
 app.ws('/websocket', function (a){
